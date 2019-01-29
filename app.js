e08790a9510fb42c606e860c1b7d7dc55c4f6b60
Nathan DeBenedetti <natedebenedetti@gmail.com>
2019-01-29 12:38:31 -0500

    added event.preventDefault() to correct page refresh issue

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(document).ready(function () { 
+$(document).ready(function () {
 
     $('form').append('<input type="submit" id="btnSubmit" value="Submit" disabled></input>') //creates a disabled button with the id btnSubmit that is labeled submit and appends it to the from element created in my index.html
 
@@ -6,15 +6,25 @@ $(document).ready(function () {
         $('#btnSubmit').removeAttr('disabled');
     });
 
-    $('body').append('<div></div>');
-
     $('#btnSubmit').click(function () { //jquery event listener
-        formVal = $('input#formTxt1').val();  //creates variable that holds the value typed into the input with id formTxt1 that I created on my index.html page.
+        event.preventDefault(); //this prevents the default form behavior of refreshing the screen from happening and breaking the code below it.
+        let formVal = $('input#formTxt1').val();  //creates variable that holds the value typed into the input with id formTxt1 that I created on my index.html page.
         alert(formVal); // and alerts that value when the button is clicked.
-        
+        $('div').append('<h2>' + formVal + '</h2>');
+
+        $('h2').mouseover(function () {
+            $('h2').
+        })
+
     });
 
-    
+    $('body').append('<div></div>');
+
+
+
+
+
+
 
 
 
